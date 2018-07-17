d21cd83895462ed5cb03e2314a06b06d6f754887
TomMilon <thomas.milon@mnhn.fr>
2018-07-17 14:43:56 +0200

    plateforme.php : stabilisation

--- _INCLUDE/fonctions.inc.php
+++ _INCLUDE/fonctions.inc.php
@@ -44,36 +44,22 @@ function html_header ($charset,$css) {
 function sqlConst ($theme,$idObjet) {
 
 if ($theme == "plateforme") {
-	$reqSql = "
-	WITH list_org as (
-		  SELECT id_ptf, outil_org as org FROM hab.outil
-	UNION SELECT id_ptf, reseau_org  as org FROM hab.reseau
-	UNION SELECT id_ptf, pilote_org  as org FROM hab.pilote
-	UNION SELECT id_ptf, interf_org  as org FROM hab.interface
-	UNION SELECT id_ptf, interf_contact_org  as org FROM hab.interface
-	), list_org_groupe as (
-	SELECT id_ptf, string_agg(list_org.org,' / ') as orglist FROM list_org GROUP BY id_ptf
-	) , liste_outil as (
-	SELECT id_ptf, string_agg(outil_nom,' / ') as toollist FROM hab.outil GROUP BY id_ptf
-	)
-	SELECT 
-		nom_region as \"Nom de la région\",
-		CASE WHEN hab_decision IS NULL THEN 'Habilitation en cours' ELSE hab_decision END as \"Statut de la plateforme\",
-		e.val_nmc as \"Dynamique de la plateforme\",
-		c3.val_nmc as \"Plateforme pérène?\",
-		crit3_desc as \"compléments sur la pérénité\",
-		c2.val_nmc as \"Charte SINP?\",
-		crit2_desc as \"compléments sur la charte\",
-		CASE WHEN charte.pj_nom IS NOT NULL THEN charte.pj_nom||' : '||charte.pj_ressource ELSE 'Pas de lien vers la charte disponible' END as \"lien vers la charte\",
-		CASE WHEN std.pj_nom IS NOT NULL THEN std.pj_nom||' : '||std.pj_ressource ELSE 'Pas standard régional' END as \"Standard de données régional SINP\",
-		c8.val_nmc as \"Échange avec la plateforme nationale?\",
-		crit8_desc as \"compléments sur les échanges\",
-		null as \"liste des jeux de données échangés\",
-		c11.val_nmc as \"validation scientifique des données?\",
-		crit11_desc as \"compléments sur la validation scientifique\",
-		CASE WHEN ref_sensi.pj_nom IS NOT NULL THEN ref_sensi.pj_nom||' : '||ref_sensi.pj_ressource ELSE 'Pas de réféentiel de sensibilité SINP' END as \"Référentiel de sensibilité\",
-		toollist as \"liste des outils\",
-		orglist as \"liste des organismes\"
+	$reqSql[0] = "
+	SELECT		
+		nom_region,
+		CASE WHEN hab_decision IS NULL THEN 'Habilitation en cours' ELSE hab_decision END as \"hab_decision\",
+		e.val_nmc as \"dynamique\",
+		c3.val_nmc \"perenne\",
+		crit3_desc \"perenne_desc\",
+		c2.val_nmc \"charte\",
+		crit2_desc \"charte_desc\",
+		CASE WHEN charte.pj_nom IS NOT NULL THEN charte.pj_nom||' : '||charte.pj_ressource ELSE 'Pas de lien vers la charte disponible' END as \"charte_pj\",
+		CASE WHEN std.pj_nom IS NOT NULL THEN std.pj_nom||' : '||std.pj_ressource ELSE 'Pas standard régional' END as \"standard\",
+		c8.val_nmc \"echange\",
+		crit8_desc \"echange_desc\",
+		c11.val_nmc \"validation\",
+		crit11_desc\"validation_desc\",
+		CASE WHEN ref_sensi.pj_nom IS NOT NULL THEN ref_sensi.pj_nom||' : '||ref_sensi.pj_ressource ELSE 'Pas de réféentiel de sensibilité SINP' END as \"ref_sensibilité\"
 	FROM hab.plateforme a
 	JOIN hab.habilitation z ON a.id_ptf = z.id_ptf
 	JOIN hab.reseau ON a.id_ptf = hab.reseau.id_ptf
@@ -85,12 +71,26 @@ if ($theme == "plateforme") {
 	LEFT JOIN hab.piece_jointe charte ON a.id_ptf = charte.id_ptf AND charte.pj_type = 'charte'
 	LEFT JOIN hab.piece_jointe std ON a.id_ptf = std.id_ptf AND std.pj_type = 'std'
 	LEFT JOIN hab.piece_jointe ref_sensi ON a.id_ptf = ref_sensi.id_ptf AND ref_sensi.pj_type = 'ref_sensi'
-	JOIN list_org_groupe ON a.id_ptf = list_org_groupe.id_ptf
-	JOIN liste_outil ON a.id_ptf = liste_outil.id_ptf
 	WHERE a.id_ptf = $idObjet
-	GROUP BY nom_region,hab_decision, e.val_nmc,c3.val_nmc, crit3_desc, c2.val_nmc,crit2_desc, charte.pj_nom, charte.pj_ressource, std.pj_nom, std.pj_ressource, ref_sensi.pj_nom, ref_sensi.pj_ressource,c8.val_nmc,crit8_desc, c11.val_nmc , crit11_desc, ref_sensi.pj_nom, ref_sensi.pj_ressource, list_org_groupe.orglist, liste_outil.toollist
+	GROUP BY nom_region,hab_decision, e.val_nmc,c3.val_nmc, crit3_desc, c2.val_nmc,crit2_desc, charte.pj_nom, charte.pj_ressource, std.pj_nom, std.pj_ressource, ref_sensi.pj_nom, ref_sensi.pj_ressource,c8.val_nmc,crit8_desc, c11.val_nmc , crit11_desc, ref_sensi.pj_nom, ref_sensi.pj_ressource
 	;
 	";
+	
+	$reqSql[1] = "
+	WITH list_org as (SELECT pilote_org  as nom, 'pilote' as role FROM hab.pilote WHERE id_ptf = $idObjet
+		UNION SELECT outil_org as nom, 'porteur d''outil' as role FROM hab.outil WHERE id_ptf = $idObjet
+		UNION SELECT reseau_org  as nom, 'tête de réseau' as role FROM hab.reseau WHERE id_ptf = $idObjet
+		UNION SELECT interf_org  as nom, 'contact données' as role FROM hab.interface WHERE id_ptf = $idObjet)
+	SELECT nom, role FROM list_org ORDER BY nom, role
+	;
+	";
+	
+	$reqSql[2] = "
+	SELECT id_outil, outil_nom FROM hab.outil WHERE id_ptf = $idObjet
+	ORDER BY outil_nom
+	;
+	";
+	
 	}
 elseif ($theme == "organisme") {
 	$reqSql = "
--- core/plateforme.php
+++ core/plateforme.php
@@ -20,33 +20,35 @@ if (!$db) fatal_error ("Impossible de se connecter au serveur PostgreSQL ".SQL_s
 //------------------------------------------------------------------------------ MAIN
 html_header ("utf-8","","");
 
-//Récupération des données
-$pgresult=pg_query ($db,sqlConst ("plateforme",$_GET["id"])) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);
-$tab = pg_fetch_all($pgresult);
-$result = $tab[0];
-// var_dump($result);
+//-----Récupération des données
+$req = sqlConst("plateforme",$_GET["id"]);
+//plateformes
+$pgresult=pg_query ($db,$req[0]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$tab = pg_fetch_all($pgresult);
+$ptf = $tab[0];
+//organismes
+$pgresult=pg_query ($db,$req[1]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$org = pg_fetch_all($pgresult);
+//tool
+$pgresult=pg_query ($db,$req[2]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$tool = pg_fetch_all($pgresult);
 
 ?>
-<h2><?php echo $result["Nom de la région"];?></h2>
-<b><a href="./question.php?id=1">Statut de la plateforme</a></b> : <?php echo $result["Statut de la plateforme"];?><BR><BR>
-<b><a href="./question.php?id=2">Dynamique de la plateforme</a></b> : <?php echo $result["Dynamique de la plateforme"];?><BR><BR>
-<b><a href="./question.php?id=3">Plateforme pérène?</a></b> : <?php echo $result["Plateforme pérène?"];?><BR>
-<?php echo $result["compléments sur la pérénité"];?><BR><BR>
-<b><a href="./question.php?id=4">Charte SINP?</a></b> : <?php echo $result["Charte SINP?"];?><BR>
-<?php echo $result["compléments sur la charte"];?><BR>
-<?php echo $result["lien vers la charte"];?><BR><BR>
-<b><a href="./question.php?id=5">Standard de données régional SINP</a></b> : <?php echo $result["Standard de données régional SINP"];?><BR><BR>
-<b><a href="./question.php?id=6">Échange avec la plateforme nationale?</a></b> : <?php echo $result["Échange avec la plateforme nationale?"];?><BR>
-<?php echo $result["compléments sur les échanges"];?><BR><BR>
-<b><a href="./question.php?id=7">validation scientifique des données?</a></b> : <?php echo $result["validation scientifique des données?"];?><BR>
-<?php echo $result["compléments sur la validation scientifique"];?><BR><BR>
-<b><a href="./question.php?id=8">Référentiel de sensibilité</a></b> : <?php echo $result["Référentiel de sensibilité"];?><BR><BR>
+<h2><?php echo $ptf["nom_region"];?></h2>
+<b><a href="./question.php?id=1">Statut de la plateforme</a></b> : <?php echo $ptf["hab_decision"];?><BR><BR>
+<b><a href="./question.php?id=2">Dynamique de la plateforme</a></b> : <?php echo $ptf["dynamique"];?><BR><BR>
+<b><a href="./question.php?id=3">Plateforme pérène?</a></b> : <?php echo $ptf["perenne"];?><BR>
+<?php echo $ptf["perenne_desc"];?><BR><BR>
+<b><a href="./question.php?id=4">Charte SINP?</a></b> : <?php echo $ptf["charte"];?><BR>
+<?php echo $ptf["charte_desc"];?><BR>
+<?php echo $ptf["charte_pj"];?><BR><BR>
+<b><a href="./question.php?id=5">Standard de données régional SINP</a></b> : <?php echo $ptf["standard"];?><BR><BR>
+<b><a href="./question.php?id=6">Échange avec la plateforme nationale?</a></b> : <?php echo $ptf["echange"];?><BR>
+<?php echo $ptf["echange_desc"];?><BR><BR>
+<b><a href="./question.php?id=7">validation scientifique des données?</a></b> : <?php echo $ptf["validation"];?><BR>
+<?php echo $ptf["validation_desc"];?><BR><BR>
+<b><a href="./question.php?id=8">Référentiel de sensibilité</a></b> : <?php echo $ptf["ref_sensibilité"];?><BR><BR>
 <b>liste des jeux de données échangés</b> travaux en cours
 <BR><BR>
 <b>Liste des outils</b><BR>
-<b><a href="./outil.php?id=8"><?php echo $result["liste des outils"];?></a></b> : <BR><BR>
-<b>liste des organismes</b><BR>
-<b><a href="./organisme.php?id=8"><?php echo $result["liste des organismes"];?></a></b> : <BR><BR>
-
-
+<?php foreach ($tool as $unit) echo "<li><a href=\"outil.php?id=".$unit["id_outil"]."\">".$unit["outil_nom"]."</a></li>";?>
+<b>Liste des organismes</b><BR>
+<?php foreach ($org as $unit) echo "<li><a href=\"organisme.php?id=".$unit["nom"]."\">".$unit["nom"]." (".$unit["role"].")</a></li>";?>
 
