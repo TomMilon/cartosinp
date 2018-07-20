63fd1d710abf1ccc69dd41e277d4def17352c72f
TomMilon <thomas.milon@mnhn.fr>
2018-07-20 16:38:32 +0200

    outil.php ajout lien fiches

--- core/outil.php
+++ core/outil.php
@@ -14,22 +14,27 @@ if (!$db) fatal_error ("Impossible de se connecter au serveur PostgreSQL ".SQL_s
 //------------------------------------------------------------------------------ MAIN
 html_header ("utf-8","","");
 
-//Récupération des données
+//---------------
+//-----Récupération des données
+// ---- outils
 $req = sqlConst("outil",$_GET["id"]);
 $pgresult=pg_query ($db,$req[0]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$tab = pg_fetch_all($pgresult);
 $result = $tab[0];
-// var_dump($result);
-
-//Récupération des données
+// ------ fonction
 $pgresult=pg_query ($db,$req[1]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$list = pg_fetch_all($pgresult);
 foreach ($list as $unit) $fct_outil[$unit["lib_nmc"]] = $unit["val_nmc"];
 // var_dump($list);
+//-----plateformes
+// $pgresult=pg_query ($db,$req[0]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$ptf = pg_fetch_all($pgresult);
+//-----organismes
+$pgresult=pg_query ($db,$req[2]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$org = pg_fetch_all($pgresult);
+
 
 ?>
 <h2><?php echo $result["outil_nom"];?></h2>
 <b><a href="./question.php?id=x">Dynamique de l'outil</a></b> : <?php echo $result["outil_dynq"];?><BR>
 <b>URL l'outil</a></b> : <a href="<?php echo $result["outil_url"];?>"><?php echo $result["outil_url"];?></a><BR>
-<b>Orgasime</b> : <?php echo $result["outil_org"];?><BR>
+<b>Organisme </b> : <?php echo $result["outil_org_nom"];?><BR>
 <?php echo $result["outil_desc"];?><BR>
 <?php 
 for ($i = 1; $i <= 19; $i++) {
@@ -38,15 +43,25 @@ for ($i = 1; $i <= 19; $i++) {
 ?>
 <BR><BR>
 
+<!-----LIEN avec les autres fiches------>
+<div id="c1" class="ptf">
 <b>Liste des plateformes</b><BR>
 <?php echo "<li><a href=\"plateforme.php?id=".$result["id_ptf"]."\">".$result["nom_region"]."</a></li>";?>
-<BR><BR>
+</div>
 
+<div id="c3" class="organisme">
 <b>Liste des organismes</b><BR>
-affaire à suivre<BR><BR>
+<?php 
+if (empty($org)) echo $valeur_non_applicable; else foreach ($org as $unit) echo "<li><a href=\"organisme.php?id=".$unit["outil_org_id"]."\">".$unit["outil_org_nom"]."</a></li>";
+?>
+</div>
 
+<div id="c2" class="jdd">
 <b>Liste des jeux de données</b><BR>
-affaire à suivre<BR><BR>
+<?php 
+?>
+</div>
+
 
 
 
