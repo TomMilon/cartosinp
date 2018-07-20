f127b79bf91d69f8c15743644e4de6f7b2677c64
TomMilon <thomas.milon@mnhn.fr>
2018-07-20 16:37:21 +0200

    plateforme.php stabilisation

--- core/plateforme.php
+++ core/plateforme.php
@@ -14,20 +14,23 @@ if (!$db) fatal_error ("Impossible de se connecter au serveur PostgreSQL ".SQL_s
 //------------------------------------------------------------------------------ MAIN
 html_header ("utf-8","","");
 
+//---------------
 //-----Récupération des données
 $req = sqlConst("plateforme",$_GET["id"]);
-//plateformes
+//-----plateformes
 $pgresult=pg_query ($db,$req[0]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$tab = pg_fetch_all($pgresult);
 $ptf = $tab[0];
-// var_dump($ptf);
 if ($ptf["hab_decision"] == "Processus non lancé") $ptf["dynamique"] =  $ptf["perenne"] =  $ptf["perenne_desc"] =  $ptf["charte"] =  $ptf["charte_desc"] = $ptf["charte_pj"] =  $ptf["standard"] =  $ptf["echange"] =  $ptf["echange_desc"] =  $ptf["validation"] = $ptf["validation_desc"] = $ptf["ref_sensibilité"] = $valeur_non_renseigne;
-
-
-//organismes
+//-----organismes
 $pgresult=pg_query ($db,$req[1]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$org = pg_fetch_all($pgresult);
-
-//tool
+//-----tool
 $pgresult=pg_query ($db,$req[2]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$tool = pg_fetch_all($pgresult);
+
+//----- référentiels
+$sql = "SELECT lib_nmc, val_nmc FROM nomenc.role_org";
+$pgresult = pg_query ($db,$sql) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$role_all = pg_fetch_all($pgresult);
+foreach ($role_all as $unit) {$ref_role[$unit["lib_nmc"]] = $unit["val_nmc"];}
+
 
 ?>
 <h2><?php echo $ptf["nom_region"];?></h2>
@@ -47,11 +50,12 @@ $pgresult=pg_query ($db,$req[2]) or fatal_error ("Erreur pgSQL : ".pg_result_err
 
 <BR><BR>
 
+
+<!-----LIEN avec les autres fiches------>
 <div id="c1" class="jdd">
 <b>Liste des jeux de données</b><BR>
-affaire à suivre<BR><BR>
-</div>
 
+</div>
 
 <div id="c3" class="outil">
 <b>Liste des outils</b><BR>
@@ -60,14 +64,9 @@ if (empty($tool)) echo $valeur_non_renseigne; else foreach ($tool as $unit) echo
 <BR><BR>
 </div>
 
-
 <div id="c2" class="organisme">
 <b>Liste des organismes</b><BR>
 <?php 
-$sql = "SELECT lib_nmc, val_nmc FROM nomenc.role_org";
-$pgresult = pg_query ($db,$sql) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);$role_all = pg_fetch_all($pgresult);
-foreach ($role_all as $unit) {$ref_role[$unit["lib_nmc"]] = $unit["val_nmc"];}
-
 if (empty($org)) echo $valeur_non_renseigne; else
 foreach ($org as $unit) 
 	{if ($unit["id"]!= null) 
