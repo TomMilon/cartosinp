1c213073d09acdddb9a5586b111b735ec1deb97e
TomMilon <thomas.milon@mnhn.fr>
2018-07-24 17:29:30 +0200

    question finition

--- core/question.php
+++ core/question.php
@@ -14,7 +14,18 @@ if (!$db) fatal_error ("Impossible de se connecter au serveur PostgreSQL ".SQL_s
 //------------------------------------------------------------------------------ MAIN
 html_header ("utf-8","","");
 
-//Récupération des données
+
+$question_gerees = array(1,2,3,4,5,6,7);
+// Question
+$sqlList["question"] = "SELECT val_nmc, '<a href=\"question.php?id='||lib_nmc||'\">'||val_nmc||'</a>' as url FROM nomenc.carto_question ORDER BY lib_nmc::integer;";
+$result=pg_query ($db,$sqlList["question"]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($result),false);
+$questions = pg_fetch_all($result);
+?>
+<h2><?php echo $questions[$_GET["id"]-1]["val_nmc"];?></h2>
+<?php
+
+if (in_array($_GET["id"],$question_gerees)){
+	//Récupération des données
 $req = sqlConst("question",$_GET["id"]);
 $pgresult=pg_query ($db,$req) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);
 while($res = pg_fetch_row($pgresult))
@@ -24,10 +35,7 @@ while($res = pg_fetch_row($pgresult))
 $sql = "SELECT insee_reg, ST_AsGeoJSON(ST_Transform(geom,4326),15,0) FROM nomenc.region_geo;";
 $pgresult=pg_query ($db,$sql) or fatal_error ("Erreur pgSQL : ".pg_result_error ($pgresult),false);
 while($res = pg_fetch_row($pgresult)) {$geo_region[$res[0]] = $res[1];}
-// Question
-$sqlList["question"] = "SELECT val_nmc, '<a href=\"question.php?id='||lib_nmc||'\">'||val_nmc||'</a>' as url FROM nomenc.carto_question ORDER BY lib_nmc::integer;";
-$result=pg_query ($db,$sqlList["question"]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($result),false);
-$questions = pg_fetch_all($result);
+
 
 //$JsonFeature
 $JsonFeature = null;
@@ -43,20 +51,7 @@ $JsonStyle .= "}";
 
 ?>
 
-<h2><?php echo $questions[$_GET["id"]-1]["val_nmc"];?></h2>
-
 <div id="mapid_big"></div>
-
-<BR><BR>
-<div id="question">
-	<h2>Les questions</h2>
-	<?php
-	$sqlList["question"] = "SELECT '<a href=\"question.php?id='||lib_nmc||'\">'||val_nmc||'</a>' FROM nomenc.carto_question ORDER BY lib_nmc::integer;";
-	$result=pg_query ($db,$sqlList["question"]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($result),false);
-	while ($row = pg_fetch_row($result))
-		echo "<li>".$row[0]."</li>";
-		?>
-</div>
 
 <script type="text/javascript">
 function onEachFeature(feature, layer) {
@@ -76,4 +71,22 @@ L.geoJSON(geojsonFeature , {onEachFeature: onEachFeature, style: function(featur
 
 
 </script>
+<?php 
+}
+else echo "Affaire à suivre";
+?>
+
+<BR><BR>
+<div id="question">
+	<h2>Les questions</h2>
+	<?php
+	$sqlList["question"] = "SELECT '<a href=\"question.php?id='||lib_nmc||'\">'||val_nmc||'</a>' FROM nomenc.carto_question ORDER BY lib_nmc::integer;";
+	$result=pg_query ($db,$sqlList["question"]) or fatal_error ("Erreur pgSQL : ".pg_result_error ($result),false);
+	while ($row = pg_fetch_row($result))
+		echo "<li>".$row[0]."</li>";
+		?>
+</div>
+
+
+
 
